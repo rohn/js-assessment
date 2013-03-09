94b8456ef0c15f4458cb8689b0852e430d0b2060
rohn <rohn.blake@helpsystems.com>
2013-03-09 14:33:10 -0600

    one more update, thought I had more time to play

--- app/functions.js
+++ app/functions.js
@@ -23,7 +23,11 @@ define(function() {
     },
 
     useArguments : function() {
-
+        var result = 0;
+        for (var i = 0; i < arguments.length; i ++) {
+            result += arguments[i];
+        }
+        return result;
     },
 
     callIt : function(fn) {
